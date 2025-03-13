cdf8f34b1b38595286ad1d12252eb5eeaae5150b
Данил Марков <markov.danil.04@mail.ru>
2025-03-13 23:06:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+
+# def sum_fn(a: float, b: float) -> float: return a + b;
+# def minus_fn(a: float, b: float) -> float: return a - b;
+#
+#
+# def calc(a: float, b: float, operation: Callable[[float, float], float]) -> float:
+#     return operation(a,b)
+#
+#
+# print(calc(3.5,4, sum_fn))
+# print(calc(3.5,4, minus_fn))
+
+# def select(input_function):
+#     def output_function():
+#         print(f"Выполнилась функция {input_function.__name__}")
+#         input_function()
+#
+#     return output_function
+#
+#
+# @select
+# def say_hello(): print("Привет")
+#
+#
+# say_hello()
